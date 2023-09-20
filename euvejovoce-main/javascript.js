4493d3dfbf47f68adc83c39ac51c5d05fb3991df
Bugmenn <125574692+Bugmenn@users.noreply.github.com>
2023-09-20 19:35:03 -0300

    Add files via upload

---
+++ euvejovoce-main/javascript.js
@@ -0,0 +1,282 @@
+function enviarResposta(numero_desafio) {
+    alert("Letras minúsculas e sem acento");
+    const resposta = prompt("...")
+
+    if (numero_desafio == 1){
+
+        if (resposta == "ser humano"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+                content = "0 ; url = tela2.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro.html"/>
+            `;
+        }
+    } else if (numero_desafio == 2){
+        if (resposta == "let the games begin"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+                content = "0 ; url = tela3.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro1.html"/>
+            `;
+        }
+    } else if (numero_desafio == 3){
+        if (resposta == "relogio"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela4.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro2.html"/>
+            `;
+        }
+    } else if (numero_desafio == 4){
+        if (resposta == "leonardo da vinci"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela9.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro3.html"/>
+            `;
+        }
+    }else if (numero_desafio == 5){
+        if (resposta == "rousseau maquiavel"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela6.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro4.html"/>
+            `;
+        }
+    }else if (numero_desafio == 6){
+        if (resposta == "gato"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela7.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro5.html"/>
+            `;
+        }
+    }else if (numero_desafio == 7){
+        if (resposta == "aravipac"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela8.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro6.html"/>
+            `;
+        }
+    }else if (numero_desafio == 8){
+        if (resposta == "paris"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela9.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro7.html"/>
+            `;
+        }
+    }else if (numero_desafio == 9){
+        if (resposta == "abril"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela10.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro8.html"/>
+            `; 
+        }
+    }else if (numero_desafio == 10){
+        if (resposta == "renascimento"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela11.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro9.html"/>
+            `;
+        }
+    }else if (numero_desafio == 11){
+        if (resposta == "lisa gherardini"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela12.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro10.html"/>
+            `;
+        }
+    }else if (numero_desafio == 12){
+        if (resposta == "jezabel"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela13.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro11.html"/>
+            `;
+        }
+    }else if (numero_desafio == 13){
+        if (resposta == "monalisa"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela14.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro12.html"/>
+            `;
+        }
+    }else if (numero_desafio == 14){
+        if (resposta == "keep swimming"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela15.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro13.html"/>
+            `;
+        }
+    }else if (numero_desafio == 15){
+        if (resposta == "sete" || "7"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela16.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro14.html"/>
+            `;
+        }
+    }else if (numero_desafio == 16){
+        if (resposta == "profecia cassandra"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela17.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro15.html"/>
+            `;
+        }
+    }else if (numero_desafio == 17){
+        if (resposta == "lv"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela18.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro16.html"/>
+            `;
+        }
+    }else if (numero_desafio == 18){
+        if (resposta == "eu sou genial"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela19.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro17.html"/>
+            `;
+        }
+    }else if (numero_desafio == 19){
+        if (resposta == "europa"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela20.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro18.html"/>
+            `;
+        }
+    }else if (numero_desafio == 20){
+        if (resposta == "seis sementes de roma"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela21.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro19.html"/>
+            `;
+        }
+    }else if (numero_desafio == 21){
+        if (resposta == "Οιδίπους"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela22.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro20.html"/>
+            `;
+        }
+    }else if (numero_desafio == 22){
+        if (resposta == "psique"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = tela23.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro21.html"/>
+            `;
+        }
+    }else if (numero_desafio == 23){
+        if (resposta == "clara"){
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = telafinal.html"/>
+            `;
+        } else {
+            document.getElementById("main").innerHTML = `
+            <meta http-equiv="refresh" 
+            content = "0 ; url = erro22.html"/>
+            `;
+        }
+}    }
